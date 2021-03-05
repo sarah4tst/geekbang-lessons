00aed089f4486f60e7b188e9754e7506481eab17
sarah <liuli.uestc@gmail.com>
2021-03-05 16:12:14 -0500

    Refactor: Pull UserServiceImpl.java to upper level package, to conform with upstream file structure.

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/service/UserServiceImpl.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/service/UserServiceImpl.java
@@ -1,12 +1,26 @@
 package org.geektimes.projects.user.service;
 
 import org.geektimes.projects.user.domain.User;
-import org.geektimes.projects.user.sql.DBConnectionManager;
+import org.geektimes.projects.user.repository.DatabaseUserRepository;
+import org.geektimes.projects.user.repository.UserRepository;
 
 public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserServiceImpl() {
+        userRepository = new DatabaseUserRepository();
+    }
 
     @Override
     public boolean register(User user) {
+        User queryUser = userRepository.getByName(user.getName());
+        if (queryUser == null) {
+            return userRepository.save(user);
+        }
         return false;
     }
 
@@ -22,11 +36,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User queryUserById(Long id) {
+        System.out.println(userRepository.getAll());
         return null;
     }
 
     @Override
     public User queryUserByNameAndPassword(String name, String password) {
-        return null;
+        return userRepository.getByNameAndPassword(name,password);
     }
 }
